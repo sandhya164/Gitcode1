197197fcef7c21c63a217ad16def61147dc10df9
raje <raje@LAPTOP-T5HPURNE>
2022-05-10 23:23:19 +0530

    second message

--- src/gitproject/FIRSTgit.java
+++ src/gitproject/FIRSTgit.java
@@ -1,5 +1,17 @@
 package gitproject;
 
-public class FIRSTgit {
 
+public class FIRSTgit
+{
+	
+	public static void main(String [] args)
+	{
+		System.out.println("first project");
+		
+		
+		System.out.println("second step ");
+		System.out.println( "third step ");
+
+
+	}
 }
